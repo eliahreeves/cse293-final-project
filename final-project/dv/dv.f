ed6885d2a66c7a23a7cbcb09d8da9daadf82a625
ericbreh <ericchuang94@gmail.com>
2025-02-05 14:25:53 -0800

    copied over template and rtl

---
+++ final-project/dv/dv.f
@@ -0,0 +1,23 @@
+
+dv/dv_pkg.sv
+
+dv/blinky_tb.sv
+
+--timing
+-j 0
+-Wall
+--assert
+--trace-fst
+--trace-structs
+--main-top-name "-"
+
+// Run with +verilator+rand+reset+2
+--x-assign unique
+--x-initial unique
+
+-Werror-IMPLICIT
+-Werror-USERERROR
+-Werror-LATCH
+
+// Specifying c++14 may be required for some compilers
+-CFLAGS -std=c++14
